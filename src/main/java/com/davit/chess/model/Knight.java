13349c54049a1245f6afb44d7b980ee18af8c5b6
Davit Ioramashvili <dioramashvili@gmail.com>
2025-04-10 21:58:51 +0400

    Knight Class added

---
+++ src/main/java/com/davit/chess/model/Knight.java
@@ -0,0 +1,39 @@
+package com.davit.chess.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Knight extends Piece {
+
+
+    public Knight(Color color, PieceType type) {
+        super(color, type);
+    }
+
+    @Override
+    public List<Move> getLegalMoves(Board board, Square from) {
+
+        List<Move> moves = new ArrayList<>();
+
+        int row = from.row();
+        int col = from.col();
+        int[][] offsets = {
+                {2, 1}, {1, 2}, {-1, 2}, {-2, 1},
+                {-2, -1}, {-1, -2}, {1, -2}, {2, -1}
+        };
+
+        for (int[] offset : offsets) {
+            int newRow = row + offset[0];
+            int newCol = col + offset[1];
+
+                if (board.isOnBoard(newRow, newCol)){
+                    Square to = new Square(newRow, newCol);
+                    if(!board.isOccupied(to) || board.getPiece(to).getColor() != this.getColor()){
+                        moves.add(new Move(from, to, this));
+                    }
+                }
+        }
+
+        return moves;
+    }
+}
