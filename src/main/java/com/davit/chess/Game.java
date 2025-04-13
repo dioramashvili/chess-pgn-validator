734fd64a13f0b9162a0d586b0df695789bffe91c
Davit Ioramashvili <dioramashvili@gmail.com>
2025-04-14 00:47:32 +0400

    Initial game logic implemented

---
+++ src/main/java/com/davit/chess/Game.java
@@ -0,0 +1,34 @@
+package com.davit.chess;
+
+import com.davit.chess.model.*;
+
+public class Game {
+    private final Board board;
+    private Color playerToMove;
+
+    public Game(Board board) {
+        this.board = new Board();
+        this.playerToMove = Color.WHITE;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Color getPlayerToMove() {
+        return playerToMove;
+    }
+
+    public void switchTurn() {
+        playerToMove = (playerToMove == Color.WHITE) ? Color.BLACK : Color.WHITE;
+    }
+
+    public boolean tryMove(Move move) {
+        if (MoveValidator.isMoveLegal(board, move, playerToMove)) {
+            board.movePiece(move);
+            switchTurn();
+            return true;
+        }
+        return false;
+    }
+}
